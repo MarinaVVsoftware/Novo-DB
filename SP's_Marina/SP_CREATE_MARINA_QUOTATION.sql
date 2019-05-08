1e704404fc5973ade942c073c1b7529f97339675
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-05-08 16:26:23 -0500

    Create marina quotation

---
+++ SP's_Marina/SP_CREATE_MARINA_QUOTATION.sql
@@ -0,0 +1,47 @@
+CREATE PROCEDURE `SP_CREATE_MARINA_QUOTATION`
+(
+    _boat_id INT,
+    _marina_quotation_status_id INT,
+    _marina_mooring_rate_id INT,
+    _arrival_date DATE,
+    _departure_date DATE,
+    _arrival_status BOOLEAN,
+    _days_stay INT, 
+    _discount_stay FLOAT(12,6),
+    _tax FLOAT(12,6),
+    _total FLOAT(12,6), 
+    _subtotal FLOAT(12,6)
+)
+
+BEGIN
+
+INSERT INTO Marina_Quotations 
+(
+    boat_id, 
+    marina_quotation_status_id, 
+    marina_mooring_rate_id,
+    arrival_date,
+    departure_date,
+    arrival_status,
+    days_stay,
+    discount_stay,
+    tax,
+    total,
+    subtotal
+)
+VALUES
+(
+    _boat_id,
+    _marina_quotation_status_id,
+    _marina_mooring_rate_id,
+    _arrival_date,
+    _departure_date,
+    _arrival_status,
+    _days_stay, 
+    _discount_stay,
+    _tax,
+    _total, 
+    _subtotal
+);
+
+END
