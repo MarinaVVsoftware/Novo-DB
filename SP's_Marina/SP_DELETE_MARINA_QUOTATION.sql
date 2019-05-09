7c769f0481a2077fbaa0f0191c9ea0252bb95350
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-05-09 08:12:59 -0500

    Stores Procedures

---
+++ SP's_Marina/SP_DELETE_MARINA_QUOTATION.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE `SP_DELETE_MARINA_QUOTATION` ( _marina_quotation_id INT )
+
+BEGIN
+
+DELETE FROM Marina_Quotations WHERE marina_quotation_id = _marina_quotation_id;
+
+END
---
+++ SP's_Marina/SP_UPDATE_MARINA_QUOTATION.sql
@@ -0,0 +1,33 @@
+CREATE PROCEDURE `SP_UPDATE_MARINA_QUOTATION`
+(
+    _boat_id INT,
+    _marina_quotation_status_id INT,
+    _marina_mooring_rate_id INT,
+    _arrival_date DATE,
+    _departure_date DATE,
+    _arrival_status BOOLEAN,
+    _days_stay INT, 
+    _discount_stay FLOAT(12,6),
+    _tax FLOAT(12,6),
+    _total FLOAT(12,6), 
+    _subtotal FLOAT(12,6)
+)
+
+BEGIN
+
+UPDATE Marina_Quotations SET  
+    boat_id = _boat_id, 
+    marina_quotation_status_id = _marina_quotation_status_id, 
+    marina_mooring_rate_id =  _marina_mooring_rate_id,
+    arrival_date = _arrival_date,
+    departure_date = _departure_date,
+    arrival_status = _arrival_status,
+    days_stay = _days_stay,
+    discount_stay = _discount_stay,
+    tax = _tax,
+    total = _total,
+    subtotal = _subtotal
+WHERE 
+    marina_quotation_id = _marina_quotation_id
+
+END
