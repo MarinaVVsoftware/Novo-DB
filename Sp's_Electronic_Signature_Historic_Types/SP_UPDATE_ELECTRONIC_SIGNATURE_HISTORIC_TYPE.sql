4e76ba98a73812b5ee090d2e00e25c67b038c09e
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-05-10 08:14:02 -0500

    Electronic signature

---
+++ Sp's_Electronic_Signature_Historic_Types/SP_UPDATE_ELECTRONIC_SIGNATURE_HISTORIC_TYPE.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE `SP_UPDATE_ELECTRONIC_SIGNATURE_HISTORIC_TYPE`
+(
+    _electronic_signature_historic_type_id INT,
+    _type VARCHAR(100)
+)
+BEGIN
+
+UPDATE Electronic_Signature_Historic_Types SET  
+    type = _type
+WHERE electronic_signature_historic_type_id = _electronic_signature_historic_type_id;
+
+END
---
+++ Sp's_Electronic_Signature_Historics/SP_CREATE_ELECTRONIC_SIGNATURE_HISTORIC.sql
@@ -0,0 +1,26 @@
+CREATE PROCEDURE `SP_CREATE_ELECTRONIC_SIGNATURE_HISTORIC`
+(
+    _electronic_signature_historic_type_id INT,
+    _client_id INT,
+    _date DATETIME,
+    _alter_responsable VARCHAR(100)
+)
+
+BEGIN
+
+INSERT INTO Electronic_Signature_Historics
+(
+    electronic_signature_historic_type_id,
+    client_id,
+    date,
+    alter_responsable
+)
+VALUES
+(
+    _electronic_signature_historic_type_id,
+    _client_id,
+    _date,
+    _alter_responsable
+);
+
+END
