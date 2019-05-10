42dda12c424316ce749b98011893e8d269d578ff
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-05-10 08:54:26 -0500

    Socket_Types Terminado.

---
+++ Boats/Store Procedures/Socket_Types/SP_Update_Socket_Type.sql
@@ -0,0 +1,13 @@
+/* SP SP_UPDATE_SOCKET_TYPE: Actualiza un socket. */
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_UPDATE_SOCKET_TYPE`(
+    _socket_type_id INT,
+    _socket_type VARCHAR(50),
+    _description VARCHAR(200)
+)
+BEGIN
+    UPDATE socket_types SET
+        socket_type = _socket_type,
+        description = _description
+    WHERE 
+        socket_type_id = _socket_type_id;
+END
--- Boats/Tables/Create_Table_Socket_Types.sql
+++ Boats/Tables/Create_Table_Socket_Types.sql
@@ -1,16 +1,20 @@
 /* Table "Sockets" - TABLA DEBIL
 Almacena la información eléctrica de los conectores de cables (sockets).
 
-socket_type_id  -> id natural
-socket_type     -> Nombre del tipo de socket
-description     -> Descripción del socket
+socket_type_id      -> id natural
+socket_type         -> Nombre del tipo de socket
+description         -> Descripción del socket
+logic_deleted       -> borrado lógico
+logic_deleted_date  -> fecha de borrado lógico
 
-PK = socket_type_id 
+PK = socket_type_id
 */
 CREATE TABLE IF NOT EXISTS socket_types(
     socket_type_id INT NOT NULL AUTO_INCREMENT,
     socket_type VARCHAR(50) UNIQUE NOT NULL,
     description VARCHAR(200),
+    logical_deleted BOOLEAN NOT NULL DEFAULT 0,
+    logical_deleted_date DATETIME DEFAULT NULL,
 
     PRIMARY KEY (socket_type_id)
 );
