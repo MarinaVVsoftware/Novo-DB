80a0e756692b0b17f93bd9e2b4d2e5853e59e017
feliperiunk <manases9403@gmail.com>
2019-05-17 08:34:59 -0500

    #8

--- Clientes/SP´s_SOCIAL_REASON/SP_CREATE_SOCIAL_REASON.sql
+++ Clientes/SP´s_SOCIAL_REASON/SP_CREATE_SOCIAL_REASON.sql
@@ -4,9 +4,10 @@ _email VARCHAR(50),
 _social_reason VARCHAR(50),
 _RCD VARCHAR(50),
 _CFDI VARCHAR(50),
-_address VARCHAR(50)
+_address VARCHAR(50),
+_status_id INT
 )
 BEGIN
-INSERT INTO social_reason(client_id,email,social_reason,RCD,CFDI,address)
-VALUE(_client_id,email,social_reason,_RCD,_CFDI,_address);
+INSERT INTO social_reason(client_id,email,social_reason,RCD,CFDI,address,status_id)
+VALUE(_client_id,email,social_reason,_RCD,_CFDI,_address,_status_id);
 END
--- Clientes/SP´s_SOCIAL_REASON/SP_UPDATE_SOCIAL_REASON.sql
+++ Clientes/SP´s_SOCIAL_REASON/SP_UPDATE_SOCIAL_REASON.sql
@@ -5,9 +5,10 @@ _email VARCHAR(50),
 _social_reason VARCHAR(50),
 _RFC VARCHAR(50),
 _CFDI VARCHAR(50),
-_addres VARCHAR(50)
+_addres VARCHAR(50),
+_status_id INT
 )
 BEGIN
-UPDATE social_reason SET client_id = _client_id,email = _email,social_reason = _social_reason,RFC = _RFC,CFDI = _CFDI,address = _address 
+UPDATE social_reason SET client_id = _client_id,email = _email,social_reason = _social_reason,RFC = _RFC,CFDI = _CFDI,address = _address,status_id = _status_id
 WHERE social_reason_id = _social_reason_id;
 END
