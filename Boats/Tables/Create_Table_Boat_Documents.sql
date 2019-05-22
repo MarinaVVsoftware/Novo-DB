eca1f607d9b6de62486c10491d4dcdc2471e6560
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-05-22 09:14:09 -0500

    SP's para endpoint GetBoatByClient
    - se añaden fixes menores a la tabla boat_documents, esto afecta ligeramente sus SP's de crud.
    - se añaden 4 SP's para ser ejecutados por la API y construir la lista de botes de un cliente.

--- Boats/Tables/Create_Table_Boat_Documents.sql
+++ Boats/Tables/Create_Table_Boat_Documents.sql
@@ -7,19 +7,15 @@ boat_id                 -> FK con la tabla "Boats"
 document_type_id        -> id del tipo de documento
 url                     -> url de la ubicación del documento
 last_update_date        -> última fecha de actualización
-logic_deleted           -> borrado lógico
-logic_deleted_date      -> fecha de borrado lógico
 
 PK = document_id 
 */
 CREATE TABLE IF NOT EXISTS boat_documents(
     boat_document_id INT NOT NULL AUTO_INCREMENT,
     boat_id INT NOT NULL,
-    boat_document_type_id INT NOT NULL,
-    url VARCHAR(250) NOT NULL,
+    boat_document_type_id INT,
+    url VARCHAR(250),
     last_update_date DATETIME,
-    logical_deleted BOOLEAN NOT NULL DEFAULT 0,
-    logical_deleted_date DATETIME DEFAULT NULL,
 
     PRIMARY KEY (boat_document_id)
 );
