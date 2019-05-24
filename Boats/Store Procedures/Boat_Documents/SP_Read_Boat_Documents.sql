e360b5b20049c61efa941e36741cfca85ba5ca70
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-05-24 12:22:35 -0500

    SP's Read de Tablas débiles: filtrado de columnas a retornar

--- Boats/Store Procedures/Boat_Documents/SP_Read_Boat_Documents.sql
+++ Boats/Store Procedures/Boat_Documents/SP_Read_Boat_Documents.sql
@@ -1,5 +1,11 @@
 /* SP SP_READ_BOAT_DOCUMENTS: Trae los documentos de barcos. */
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_READ_BOAT_DOCUMENTS`()
 BEGIN
-    SELECT * FROM boat_documents;
+    SELECT
+        boat_document_id,
+        boat_id,
+        boat_document_type_id,
+        url,
+        last_update_date 
+    FROM boat_documents;
 END
