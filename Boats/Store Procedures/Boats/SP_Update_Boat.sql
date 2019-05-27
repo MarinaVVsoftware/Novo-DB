54427a6b5f6b91212654dce99d0c33909c06fa71
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-05-27 16:15:46 -0500

    Update de Boats
    jiji olvidé crear este SP

--- Boats/Store Procedures/Boats/SP_Update_Boat.sql
+++ Boats/Store Procedures/Boats/SP_Update_Boat.sql
@@ -1,7 +1,7 @@
 /* SP SP_UPDATE_BOAT: Actualiza un row de Barco. */
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_UPDATE_BOAT`(
-    _boat_id INT,
     _client_id INT,
+    _boat_name VARCHAR(100),
     _name VARCHAR(100),
     _model VARCHAR(100),
     _loa DECIMAL(6,2),
@@ -9,13 +9,41 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_UPDATE_BOAT`(
     _beam DECIMAL(6,2)
 )
 BEGIN
-    UPDATE boats SET
-        client_id = _client_id,
-        name = _name,
-        model = _model,
-        loa = _loa,
-        draft = _draft,
-        beam = _beam
-    WHERE 
-        boat_id = _boat_id;
+    /* verifica que exista el cliente. de lo contrario tira una excepción. */
+    IF NOT EXISTS (SELECT 1 FROM Clients WHERE client_id = _client_id) THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Client was not found. Can\'t read boats without a client_id.";
+    END IF;
+
+     /* verifica que exista el bote. de lo contrario tira una excepción. */
+    IF NOT EXISTS (SELECT 1 FROM Boats WHERE name = _boat_name) THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Boat doesn\'t exist.';
+    END IF;
+
+    SELECT boat_id INTO @boat FROM boats WHERE name = _boat_name;
+
+     /* Verifica si el bote tiene un engine con ese id. de lo contrario tira una excepción */
+    IF NOT EXISTS (
+        SELECT 1 FROM boats 
+        WHERE client_id = _client_id 
+        AND boat_id = @boat
+    ) 
+    THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Doesn\'t exist that boat related with that client.';
+    ELSE
+        UPDATE boats SET
+            client_id = _client_id,
+            name = _name,
+            model = _model,
+            loa = _loa,
+            draft = _draft,
+            beam = _beam
+        WHERE 
+            boat_id = @boat;
+    END IF;
 END
