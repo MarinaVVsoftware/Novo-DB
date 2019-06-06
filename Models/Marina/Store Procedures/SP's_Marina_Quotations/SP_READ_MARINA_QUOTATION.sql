ad826275d131fe909960f0f320b8a1dbcf2ed3e6
Manuel Gutierrez <manu.gtzp@gmail.com>
2019-06-06 16:08:21 -0500

    dev quotation

--- Models/Marina/Store Procedures/SP's_Marina_Quotations/SP_READ_MARINA_QUOTATION.sql
+++ Models/Marina/Store Procedures/SP's_Marina_Quotations/SP_READ_MARINA_QUOTATION.sql
@@ -2,15 +2,23 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_READ_MARINA_QUOTATION`(
     _quotation_id INT
 )
 BEGIN
-	SET @quotation_period := 'Cotización Mensual';
-    SELECT IF(semiannual_quotation = 1, @quotation_period := 'Cotización Semestral', @quotation_period) FROM marina_quotations WHERE marina_quotation_id = _quotation_id;
-    SELECT IF(annual_quotation = 1, @quotation_period := 'Cotización Anual', @quotation_period) FROM marina_quotations WHERE marina_quotation_id = _quotation_id;
-    
+	SET @quotation_period := NULL;
+    SELECT IF(monthly_quotation = 1, @quotation_period := 'Cotización Mensual', @quotation_period) INTO @monthly_quotation FROM marina_quotations WHERE marina_quotation_id = _quotation_id;
+    SELECT IF(semiannual_quotation = 1, @quotation_period := 'Cotización Semestral', @quotation_period) INTO @semiannual_quotation FROM marina_quotations WHERE marina_quotation_id = _quotation_id;
+    SELECT IF(annual_quotation = 1, @quotation_period := 'Cotización Anual', @quotation_period) INTO @annual_quotation FROM marina_quotations WHERE marina_quotation_id = _quotation_id;
+		
     SELECT
         mq.marina_quotation_id,
         mq.boat_id,
         mq.marina_quotation_status_id,
         mq.marina_quotation_electricity_id,
+        mqe.electricity_tariff,
+        mqe.total_days AS electricity_total_days,
+        mqe.discount_electricity_percentage,
+        mqe.currency_amount AS currency_electricity_amount,
+        mqe.tax AS tax_electricity,
+        mqe.total AS total_electricity,
+        mqe.subtotal AS subtotal_electricity,
         bt.name AS boat_name,
         mq.loa as boat_loa,
         ct.name AS client_name,
@@ -35,6 +43,7 @@ BEGIN
     INNER JOIN clients ct ON bt.client_id = ct.client_id
     INNER JOIN marina_quotation_status mqs ON mq.marina_quotation_status_id = mqs.marina_quotation_status_id
     INNER JOIN marina_mooring_tariff mmt ON mq.marina_mooring_tariff_id = mmt.marina_mooring_tariff_id
+    LEFT JOIN marina_quotation_electricity mqe ON mq.marina_quotation_electricity_id = mqe.marina_quotation_electricity_id
     WHERE mq.marina_quotation_id = _quotation_id;
 
 END
