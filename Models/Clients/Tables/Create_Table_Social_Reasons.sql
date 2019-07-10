c820adb8ba3a854dd1bffab7d86a1593f49a4a12
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-10 16:56:43 -0500

    Fixes de orden y set de unique a rfc

--- Models/Clients/Tables/Create_Table_Social_Reasons.sql
+++ Models/Clients/Tables/Create_Table_Social_Reasons.sql
@@ -3,10 +3,10 @@ Almacena las razones sociales de los clientes.
 
 social_reason_id    -> ID natural
 client_id           -> FK con cliente
-email               -> Email de la razón social
-social_reason       -> Razón social
 rfc                 -> RFC
+social_reason       -> Razón social
 cfdi                -> CFDI
+email               -> Email de la razón social
 address             -> Dirección asociada a la razón social
 logic_deleted       -> Borrado lógico
 logic_deleted_date  -> Fecha de borrado lógico
@@ -16,10 +16,10 @@ PK = social_reason_id
  CREATE TABLE IF NOT EXISTS social_reasons (
 	social_reason_id INT NOT NULL AUTO_INCREMENT,
     client_id INT NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    social_reason VARCHAR(100) NOT NULL,
-    rfc VARCHAR(50) NOT NULL,
+    rfc VARCHAR(50) UNIQUE NOT NULL,
+    social_reason VARCHAR(200) NOT NULL,
     cfdi VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     address VARCHAR (300) NOT NULL,
     logical_deleted BOOLEAN NOT NULL DEFAULT 0,
     logical_deleted_date DATETIME DEFAULT NULL,
