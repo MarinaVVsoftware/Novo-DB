8591e7060454474c9867679079c0b449a0b62586
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-22 16:54:15 -0500

    Manejo de error en PatchMarinaAmount

--- Models/Clients/Store Procedures/Electronic_Wallet/SP_ElectronicWallet_PatchMarinaAmount.sql
+++ Models/Clients/Store Procedures/Electronic_Wallet/SP_ElectronicWallet_PatchMarinaAmount.sql
@@ -4,6 +4,17 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ElectronicWallet_PatchMarinaAmou
     _marina_amount DECIMAL(12,4)
 )
 BEGIN
+    /* verifica que exista el cliente. de lo contrario tira una excepción. */   
+    IF NOT EXISTS (
+        SELECT 1 FROM clients
+        WHERE client_id = _client_id
+        AND logical_deleted = 0
+    )
+    THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE "45000";
+    END IF;
+
     UPDATE electronic_wallet SET 
         marina_amount = _marina_amount
     WHERE client_id = _client_id;
