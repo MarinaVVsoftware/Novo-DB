3c85ea98325aec104b5cb7a86dbebaed62e9ad5f
Eduardo Hidalgo <IngEduardoHidalgo@hotmail.com>
2019-07-23 09:24:52 -0500

    Implementación de Fetch en Clients

--- Models/Clients/Scripts/Drop_SPs_Clients.sql
+++ Models/Clients/Scripts/Drop_SPs_Clients.sql
@@ -13,7 +13,7 @@ DROP PROCEDURE IF EXISTS SP_Clients_PutClient;
 /* table electronic_wallet */
 DROP PROCEDURE IF EXISTS SP_ElectronicWallet_GetWallet;
 DROP PROCEDURE IF EXISTS SP_ElectronicWallet_PatchMarinaAmount;
-DROP PROCEDURE IF EXISTS SP_ElectronicWallet_PostWallet;
+DROP PROCEDURE IF EXISTS SP_ElectronicWallet_PutWallet;
 
 /* table electronic_wallet_historic */
 DROP PROCEDURE IF EXISTS SP_ElectronicWalletHistoric_GetAllHistoric;
--- Models/Clients/Store Procedures/Electronic_Wallet/SP_ElectronicWallet_PostWallet.sql
+++ Models/Clients/Store Procedures/Electronic_Wallet/SP_ElectronicWallet_PostWallet.sql
@@ -1,9 +1,30 @@
-/* SP SP_ElectronicWallet_PostWallet: Crea el monedero de un cliente,
+/* SP SP_ElectronicWallet_PutWallet: Crea el monedero de un cliente,
 solo sucede al crear al cliente por lo que no es necesario validar nada. */
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ElectronicWallet_PostWallet`(
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ElectronicWallet_PutWallet`(
     _client_id INT
 )
 BEGIN
+    /* verifica que exista el cliente. de lo contrario tira una excepción. */   
+    IF NOT EXISTS (
+        SELECT 1 FROM clients
+        WHERE client_id = _client_id
+        AND logical_deleted = 0
+    )
+    THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE "45000";
+    END IF;
+
+        /* verifica que no exista ya el monedero. de lo contrario tira una excepción. */   
+    IF EXISTS (
+        SELECT 1 FROM electronic_wallet
+        WHERE client_id = _client_id
+    )
+    THEN
+        /* Arroja un error customizado */
+        SIGNAL SQLSTATE "45001";
+    END IF;
+
     INSERT INTO electronic_wallet (
         client_id
     )
